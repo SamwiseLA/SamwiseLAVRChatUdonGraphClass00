26c2e6b54bdd2913920391be816147c8bdfde1c6
SamwiseLA <github@samwiser.com>
2024-06-25 15:12:31 -0700

    Set To Correct Directory

---
+++ Assets/Editor/CopyEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using System.IO;
+[InitializeOnLoadAttribute]
+public static class CopyEditor
+{
+    static CopyEditor()
+    {
+        EditorApplication.playModeStateChanged += LogPlayModeState;
+    }
+ 
+    private static void LogPlayModeState(PlayModeStateChange state)
+    {
+        if (File.Exists("QualitySettings.asset"))
+        {
+            File.Delete("ProjectSettings/QualitySettings.asset");
+            File.Copy("QualitySettings.asset", "ProjectSettings/QualitySettings.asset");
+        }
+    }
+ 
+}
